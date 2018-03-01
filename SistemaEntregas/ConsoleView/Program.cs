c8394fc10c2bf565e43fe9313ef50ed81a648262
Nicolas <nicolasbiletzki@gmail.com>
2018-02-28 22:17:55 -0300

    Alterando classe Pessoa para Abstract (NOVO)

--- SistemaEntregas/ConsoleView/Program.cs
+++ SistemaEntregas/ConsoleView/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleView
     {
         static void Main(string[] args)
         {
+            Pessoa p = new Pessoa();
 
         }
     }
--- SistemaEntregas/Modelos/Pessoa.cs
+++ SistemaEntregas/Modelos/Pessoa.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelos
 {
-    public class Pessoa
+    public abstract class Pessoa
     {
         public int PessoaID { get; set; }
         public string Nome { get; set; }
