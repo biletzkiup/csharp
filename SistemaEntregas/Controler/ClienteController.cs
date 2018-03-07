769834a0d376e040e19b09700c75bab42fda8b8f
Nicolas <nicolasbiletzki@gmail.com>
2018-03-07 20:18:05 -0300

    ADD REFERENCIA MODELOS NO CONTROLLER

--- SistemaEntregas/Controler/ClienteController.cs
+++ SistemaEntregas/Controler/ClienteController.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Controler
 {
-    public class Class1
+    public class ClienteController
     {
+
+        public void SalvarCliente(Cliente cliente)
+        {
+
+        }
+
+
     }
 }
